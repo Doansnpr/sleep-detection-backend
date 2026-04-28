072f0fa6eadb2ab41c1b97b2721c07f3391099f2
Doansnpr <doansriwashin45.com>
2026-04-28 12:22:20 +0700

    mengubah inputan url menjadi choose file

--- config/database.php
+++ config/database.php
@@ -136,13 +136,10 @@ return [
         ],
 
         'mongodb' => [
-    'driver'   => 'mongodb',
-    'host'     => env('DB_HOST', '127.0.0.1'),
-    'port'     => env('DB_PORT', 27017),
-    'database' => env('DB_DATABASE', 'noctura'), // ← pastikan ini 'noctura'
-    'username' => env('DB_USERNAME', ''),
-    'password' => env('DB_PASSWORD', ''),
-],
+            'driver'   => 'mongodb',
+            'dsn'      => env('DB_URI'),
+            'database' => env('DB_DATABASE', 'noctura'),
+        ],
 
     ],
      
