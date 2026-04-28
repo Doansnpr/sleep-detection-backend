a0b46d303e0804918730a03dbee117a7efdc44dc
Doansnpr <doansriwashin45@gmail.com>
2026-04-28 13:26:55 +0700

    menyelesaikan error  sanctum

--- config/database.php
+++ config/database.php
@@ -112,45 +112,12 @@ return [
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
-        'mongodb' => [
-            'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', '127.0.0.1'),
-            'port'     => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'noctura'),
-            'options'  => [
-                // jika memakai Atlas, Anda bisa menambahkan parameter lain di sini
-                // 'tls' => true,
-            ],
-        ], 
-
-        'mongodb' => [
-            'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', '127.0.0.1'),
-            'port'     => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'noctura'),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
-            'options'  => [
-                
-            ],
-        ],
 
         'mongodb' => [
             'driver'   => 'mongodb',
             'dsn'      => env('DB_URI'),
             'database' => env('DB_DATABASE', 'noctura'),
         ],
-
-        'mongodb' => [
-        'driver'   => 'mongodb',
-        'host'     => env('DB_HOST', '127.0.0.1'),
-        'port'     => env('DB_PORT', 27017),
-        'database' => env('DB_DATABASE', 'noctura'),
-        'options'  => [
-            // jika memakai Atlas, Anda bisa menambahkan parameter lain di sini
-            // 'tls' => true,
-        ],
-    ],
 
     ],
      
